2bd3843bc76c6c62cbedc960c5a0cfb7470869a0
Crois0509 <crois0509@naver.com>
2024-07-10 15:20:04 +0900

    [Round 2/Step 2] - Crois

--- CodeStarterCamp_Week2/main.swift
+++ CodeStarterCamp_Week2/main.swift
@@ -1,12 +1,22 @@
-//
-//  main.swift
-//  CodeStarterCamp_Week2
-//
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
-//
+var randomNumberOfLotto: Set<Int> = Set(1...45)
+var winningNumberOfLotto: [Int] = []
+var myLottoNumbers: [Int] = [1, 2, 3, 4, 5, 6]
 
-import Foundation
+func randomNumberGeneratorOfLotto() {
+    while randomNumberOfLotto.count > 6 {
+        randomNumberOfLotto.removeFirst()
+    }
+    
+    for counts in randomNumberOfLotto {
+        winningNumberOfLotto.append(counts)
+    }
 
-print("Hello, World!")
+    if winningNumberOfLotto == myLottoNumbers {
+        print("축하합니다! 겹치는 번호는 \(myLottoNumbers) 입니다!")
+    }else {
+        print("아쉽지만 겹치는 번호가 없습니다.")
+    }
+}
+
+randomNumberGeneratorOfLotto()
 
